7a30309a5a22a93c2a09e53754191d037cd1d385
Marcel Noe <marcel.noe@tngtech.com>
2014-01-19 01:30:26 +0100

    can now encrypt & decrypt

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
-aes:
+aes: aes.c
 	gcc -maes -mfpmath=sse -mmmx -msse -msse2 aes.c -o aes
--- aes.c
+++ aes.c
@@ -14,62 +14,113 @@ __m128i aes128_keyexpand(__m128i key, __m128i keygened){
 
 #define KEYEXP(K, I) aes128_keyexpand(K, (__m128i)_mm_aeskeygenassist_si128(K, I))
 
-void encrypt(char *, char *, char *);
+void encrypt128(char * message, __m128i * K, char * out);
+void decrypt128(char * encrypted, __m128i * K, char * out);
+void expandkey128(char * key, __m128i * K);
+void p(char *, char *);
 
 void main(){
-    char key[17] = {'S', 'e', 'c', 'r', 'e', 't'};
-    char message[17] =  {'C', 'l', 'e', 'a', 'r', 't', 'e', 'x', 't'};
-    char out[17];
+    char key[16] = {'S', 'e', 'c', 'r', 'e', 't', 's', 'F', 'o'};
+    char message[16] =  {'C', 'l', 'e', 'a', 'r', 't', 'e', 'x', 't'};
+    char out[16];
+    char decrypted[16];
+    char buf[17];
 
-    encrypt(message, key, out);
+    __m128i K[21];
+    expandkey128(key, K);
+    encrypt128(message, K, out);
+    decrypt128(out, K, decrypted);
 
-    // Terminate strings
-    key[16] = '\0';
-    out[16] = '\0';
-    message[16] = '\0';
-
-    printf("Message: %s\n", message);
-    printf("Key: %s\n", key);
-    printf("Crypt: %s\n", out);
+    p("Message", message);
+    p("Key", key);
+    p("Crypt", out);
+    p("Decrypted", decrypted);
 }
 
-void encrypt(char * message, char * key, char * out){
+void p(char * message, char * param){
+    char buf[17];
+    memcpy(buf, param, 16*sizeof(char));
+    buf[16] = '\0';
+    printf("%s: %s\n", message, buf);
+}
+
+void expandkey128(char * key, __m128i * K){
     // Key expansion: 
     // Generate key for each round
     // 128bit Keylength -> 11 rounds
     __m128i * _k = (__m128i *)key;
-    __m128i K0  = _mm_load_si128((__m128i *)(_k));
-    __m128i K1  = KEYEXP(K0, 0x01);
-    __m128i K2  = KEYEXP(K1, 0x02);
-    __m128i K3  = KEYEXP(K2, 0x04);
-    __m128i K4  = KEYEXP(K3, 0x08);
-    __m128i K5  = KEYEXP(K4, 0x10);
-    __m128i K6  = KEYEXP(K5, 0x20);
-    __m128i K7  = KEYEXP(K6, 0x40);
-    __m128i K8  = KEYEXP(K7, 0x80);
-    __m128i K9  = KEYEXP(K8, 0x1B);
-    __m128i K10 = KEYEXP(K9, 0x36);
+    K[0]  = _mm_load_si128((__m128i *)(_k));
+    K[1]  = KEYEXP(K[0], 0x01);
+    K[2]  = KEYEXP(K[1], 0x02);
+    K[3]  = KEYEXP(K[2], 0x04);
+    K[4]  = KEYEXP(K[3], 0x08);
+    K[5]  = KEYEXP(K[4], 0x10);
+    K[6]  = KEYEXP(K[5], 0x20);
+    K[7]  = KEYEXP(K[6], 0x40);
+    K[8]  = KEYEXP(K[7], 0x80);
+    K[9]  = KEYEXP(K[8], 0x1B);
+    K[10] = KEYEXP(K[9], 0x36); // Used for last round of encryption
+                                // and first round of decryption
 
+    // Decryption keys
+    K[11] = _mm_aesimc_si128(K[9]);
+    K[12] = _mm_aesimc_si128(K[8]);
+    K[13] = _mm_aesimc_si128(K[7]);
+    K[14] = _mm_aesimc_si128(K[6]);
+    K[15] = _mm_aesimc_si128(K[5]);
+    K[16] = _mm_aesimc_si128(K[4]);
+    K[17] = _mm_aesimc_si128(K[3]);
+    K[18] = _mm_aesimc_si128(K[2]);
+    K[19] = _mm_aesimc_si128(K[1]);
+    K[20] = K[0];
+}
+
+void encrypt128(char * message, __m128i * K, char * out){
     // Load message into m
     __m128i m = _mm_load_si128((const __m128i *) message);
 
     // 1. Round = message XOR key
-    m = _mm_xor_si128(m, K0);
+    m = _mm_xor_si128(m, K[0]);
 
     // 9 rounds of AESENC
-    m = _mm_aesenc_si128(m, K1);
-    m = _mm_aesenc_si128(m, K2);
-    m = _mm_aesenc_si128(m, K3);
-    m = _mm_aesenc_si128(m, K4);
-    m = _mm_aesenc_si128(m, K5);
-    m = _mm_aesenc_si128(m, K6);
-    m = _mm_aesenc_si128(m, K7);
-    m = _mm_aesenc_si128(m, K8);
-    m = _mm_aesenc_si128(m, K9);
+    m = _mm_aesenc_si128(m, K[1]);
+    m = _mm_aesenc_si128(m, K[2]);
+    m = _mm_aesenc_si128(m, K[3]);
+    m = _mm_aesenc_si128(m, K[4]);
+    m = _mm_aesenc_si128(m, K[5]);
+    m = _mm_aesenc_si128(m, K[6]);
+    m = _mm_aesenc_si128(m, K[7]);
+    m = _mm_aesenc_si128(m, K[8]);
+    m = _mm_aesenc_si128(m, K[9]);
 
     // Last round is done with AESENCLAST
-    m = _mm_aesenclast_si128(m, K10);
+    m = _mm_aesenclast_si128(m, K[10]);
 
     // Store result in out variable
     _mm_store_si128((__m128i *) out, m);
+}
+
+void decrypt128(char * encrypted, __m128i * K, char * out){
+    // Load encrypted message into c 
+    __m128i c = _mm_load_si128((const __m128i *) encrypted);
+
+    // 1. Round = crypt XOR key
+    c = _mm_xor_si128(c, K[10]);
+
+    // 9 rounds of AESDEC
+    c = _mm_aesdec_si128(c, K[11]);
+    c = _mm_aesdec_si128(c, K[12]);
+    c = _mm_aesdec_si128(c, K[13]);
+    c = _mm_aesdec_si128(c, K[14]);
+    c = _mm_aesdec_si128(c, K[15]);
+    c = _mm_aesdec_si128(c, K[16]);
+    c = _mm_aesdec_si128(c, K[17]);
+    c = _mm_aesdec_si128(c, K[18]);
+    c = _mm_aesdec_si128(c, K[19]);
+
+    // Last round is done with AESDECLAST
+    c = _mm_aesdeclast_si128(c, K[20]);
+
+    // Store result in out variable
+    _mm_store_si128((__m128i *) out, c);
 }
